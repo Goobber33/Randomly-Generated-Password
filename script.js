7eb20c5a137c53bc1cd5c98dfb58aaa04e08a69c
Kyle Parks <kyleparks@Kyles-MacBook-Pro.local>
2023-01-12 14:13:25 -0800

    Added variables and a variable for the lower case characters.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber
+var confirmSpecial;
+var userChoices;
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var blankUpper = [];
+var toUpper = function (x) {
+  return x.toUpperCase;
+};
 
 // Write password to the #password input
 function writePassword() {
